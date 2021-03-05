d21c5e84e1fb14e08e66ae0348f761bd04d32a03
vitaliiholubinka <holybinka-1996@outlook.com>
2021-03-05 18:17:20 +0200

    Updates

---
+++ precache-manifest.4f5d30290bca54a9d444d49824706b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68aa6d0e4931fb69c526a23d51d23473",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ca487c2a0d2e0bcdb1c",
+    "url": "./static/css/main.3121fd71.chunk.css"
+  },
+  {
+    "revision": "1ae3661e6638562f634e",
+    "url": "./static/js/2.47bff1d9.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.47bff1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca487c2a0d2e0bcdb1c",
+    "url": "./static/js/main.96eac395.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
